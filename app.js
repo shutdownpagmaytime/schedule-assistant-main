c049906e603173dfd937d26c452310eb7ca70550
shutdownpagmaytime <84158907+shutdownpagmaytime@users.noreply.github.com>
2021-05-21 06:06:55 +0800

    Update app.js

--- app.js
+++ app.js
@@ -13,11 +13,10 @@
 //   console.log(`Server running at http://${hostname}:${port}/`);
 // });
 var http = require('http');
-var port = process.env.port || 1337;
+var port = process.env.port || 3000;
 http.createServer(function (req, res) {
   if(req.url === '/app/')
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.statusCode = 200;
   res.end('Hello World\n');
 }).listen(port);
 
@@ -26,26 +25,26 @@ var bodyParser = require("body-parser"),
 express        = require("express"),
 axios          = require('axios'),
 app            = express.Router();
-pythonShell = require('python-shell');
+//pythonShell = require('python-shell');
 //router = express.Router();
-var options = {
-  pythonPath: 'D:/home/python27/python',
-  scriptPath: 'D:/home/site/wwwroot'
-  // args:
-  // [
-  //     req.query.term,
-  //     req.params.id,
-  //     req.session.user.searchName,
-  //     req.session.user.searchKey
-  // ]
-};
-pythonShell.run('/python_server/app.py', options, function (err, data) {
-  if (err) throw err;
-  // results is an array consisting of messages collected during execution
-  var values = JSON.parse(data[0]);
-  resultsByRel = values;
-  console.log('results: %j', resultsByRel);
-});
+// var options = {
+//   pythonPath: 'D:/home/python27/python',
+//   scriptPath: 'D:/home/site/wwwroot'
+//   // args:
+//   // [
+//   //     req.query.term,
+//   //     req.params.id,
+//   //     req.session.user.searchName,
+//   //     req.session.user.searchKey
+//   // ]
+// };
+// pythonShell.run('/python_server/app.py', options, function (err, data) {
+//   if (err) throw err;
+//   // results is an array consisting of messages collected during execution
+//   var values = JSON.parse(data[0]);
+//   resultsByRel = values;
+//   console.log('results: %j', resultsByRel);
+// });
 
 // app.get('/', function(req, res, next) {
 //   res.send(resultsByRel);
